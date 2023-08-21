c67682415562e4659f0b21399ad0a37656599c06
EdGA9001 <edward.grant-adamson@students.plymouth.ac.uk>
2023-08-21 08:24:59 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UsernameModel userFileModel = new UsernameModel();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUsername(string username, string password)
+        {
+            var result = userFileModel.Register(username, password);
+
+            if (!result)
+            {
+                return Json(new { success = false, message = "Username already exists or you left a feild empty." });
+            }
+
+            return Json(new { success = true, message = "You're registered!" });
+        }
+
+        [HttpPost]
+        public IActionResult ValidateUser(string username, string password)
+        {
+            var isValid = userFileModel.ValidateUser(username, password);
+
+            if (isValid)
+            {
+                return Json(new { success = true, message = "Welcome, " + username + "!" });
+            }
+
+            else
+            {
+                return Json(new { success = false, message = "Could not find username or password. Please try again." });
+            }
+        }
+    }
+}
---
+++ Models/UsernameModel.cs
@@ -0,0 +1,40 @@
+﻿namespace WebApplication5.Models
+{
+    public class UsernameModel
+    {
+        private const string UserStorage = "ValidUsernames.txt";
+
+        public bool Register(string username, string password)
+        {
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            var users = File.ReadAllLines(UserStorage);
+
+            if (users.Any(line => line.Split(',')[0].Equals(username, StringComparison.OrdinalIgnoreCase)))
+            {
+                return false;
+            }
+
+            File.AppendAllText(UserStorage, $"{username},{password}\n");
+
+            return true;
+        }
+
+        public bool ValidateUser(string username, string password)
+        {
+            var users = File.ReadAllLines(UserStorage);
+
+            return users.Any(line =>
+            {
+                var parts = line.Split(',');
+
+                return parts[0].Equals(username, StringComparison.OrdinalIgnoreCase) && parts[1] == password;
+            }
+            );
+        }
+    }
+}
